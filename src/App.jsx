0b1ced091315c3848f74f56393b201752ef1eacf
Farul Ahmad Wananda <farulahmad2002@gmail.com>
2024-06-28 23:21:02 +0700

    feat: implement atomic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import Button from './components/Elements/Button'
+import LoginPage from './pages/login'
+import RegisterPage from './pages/register'
+
 
 function App() {
   return (
     <>
-      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-        <div className="flex gap-x-3">
-          <Button variant="bg-blue-900">Login</Button>
-          <Button variant="bg-slate-700">Register</Button>
-          <Button variant="bg-black">Logout</Button>
-          <Button></Button>
-        </div>
-      </div >
+      <div className="flex justify-center min-h-screen items-center">
+        {/* <LoginPage /> */}
+        <RegisterPage />
+      </div>
     </>
   )
 }
---
+++ src/pages/login.jsx
@@ -0,0 +1,10 @@
+import AuthLayout from "../components/Layouts/AuthLayouts";
+import FormLogin from "../components/Fragments/FormLogin";
+
+const LoginPage = () => {
+  return (
+    <AuthLayout title="Login"><FormLogin /></AuthLayout>
+  )
+}
+
+export default LoginPage;
